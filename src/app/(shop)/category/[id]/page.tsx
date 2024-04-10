d456a244223a8b12bbf0ed08a161d8cecc2c03c2
Marcos Hernan Parella <marcosparella2000@gmail.com>
2024-04-09 23:33:36 -0300

    Cosas nuevas

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,19 +1,34 @@
-import { notFound } from 'next/navigation';
+import { ProductGrid, Title } from '@/components';
 
+import { initialData } from '@/seed/seed';
+
+import { Category } from '@/interfaces';
+
+const seedProducts = initialData.products;
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
 export default function ({ params }: Props) {
   const { id } = params;
-
-  if (id === 'kids') notFound();
+  const products = seedProducts.filter((product) => product.gender === id);
+  const labels: Record<Category, string> = {
+    kid: 'para niños',
+    men: 'para hombres',
+    unisex: 'para todos',
+    women: 'para mujeres',
+  };
 
   return (
-    <div>
-      <h1>Category Page</h1>
-    </div>
+    <>
+      <Title
+        title={`Artículos de ${labels[id]}`}
+        subtitle='Todos los productos'
+        className='mb-2'
+      />
+      <ProductGrid products={products} />
+    </>
   );
 }
